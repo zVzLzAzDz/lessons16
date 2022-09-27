21562dbe649b119115bb25b00b0e2bfb39e2d038
Vlad <103254700+zVzLzAzDz@users.noreply.github.com>
2022-09-27 16:28:53 +0300

    Add @test in DragonLogicTest

--- test/by/itstep/vlad/model/logic/DragonLogicTest.java
+++ test/by/itstep/vlad/model/logic/DragonLogicTest.java
@@ -8,7 +8,7 @@ public class DragonLogicTest {
 
 
     @Test
-    public void testFirstPeriodPositive(){
+    public void testFirstPeriodPositive() {
 
         //arrange
         Dragon dragon = new Dragon();
@@ -23,7 +23,7 @@ public class DragonLogicTest {
     }
 
     @Test
-    public void testFirstPeriodNegative(){
+    public void testFirstPeriodNegative() {
 
         //arrange
         Dragon dragon = new Dragon();
@@ -38,7 +38,7 @@ public class DragonLogicTest {
     }
 
     @Test
-    public void testFirstBoundPositive(){
+    public void testFirstBoundPositive() {
 
         //arrange
         Dragon dragon = new Dragon();
@@ -49,6 +49,83 @@ public class DragonLogicTest {
 
         int actual = DragonLogic.calculateHeads(dragon);
 
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSecondPeriodPositive() {
+        //arrange
+        Dragon dragon = new Dragon();
+        dragon.age = 130;
+        int expected = 363;
+
+        //act
+        int actual = DragonLogic.calculateHeads(dragon);
+
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testSecondPeriodNegative() {
+//        arrange
+        Dragon dragon = new Dragon();
+        dragon.age = -110;
+        int expected = -1;
+
+//        act
+
+        int actual = DragonLogic.calculateHeads(dragon);
+        Assert.assertEquals(expected, actual);
+
+
+    }
+
+    @Test
+    public void testSecondBoundPositive() {
+        // arrange
+        Dragon dragon = new Dragon();
+        dragon.age = DragonLogic.SECOND_PERIOD;
+        int expected = 503;
+        //act
+        int actual = DragonLogic.calculateHeads(dragon);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testFinalPeriodPositive() {
+
+        //arrange(организовывать)
+        Dragon dragon = new Dragon();
+        dragon.age = 205;
+        int expected = 508;
+
+        // act
+        int actual = DragonLogic.calculateHeads(dragon);
+
+        Assert.assertEquals(expected, actual);
+
+
+    }
+
+    @Test
+    public void testFinalPeriodNegative() {
+        //arrange
+        Dragon dragon = new Dragon();
+        dragon.age = -219;
+        int expected = -1;
+        // act
+        int actual = DragonLogic.calculateHeads(dragon);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testFinalBoundPositive() {
+        Dragon dragon = new Dragon();
+        dragon.age = DragonLogic.FIRST_PERIOD + DragonLogic.SECOND_PERIOD;
+        int expected = 603;
+        //act
+        int actual = DragonLogic.calculateHeads(dragon);
         Assert.assertEquals(expected, actual);
     }
 }
